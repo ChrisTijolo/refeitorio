ab7099f2c4dff67d5f0e6d8909843468ca830594
aluno <ilancristhian@gmail.com>
2024-11-27 22:36:37 -0300

    Versão 0.0.1

---
+++ userApp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+class UsuarioForm(UserCreationForm):
+    FIRST_NAME_CHOICES = [
+        ('Aluno', 'Aluno'),
+        ('Servidor', 'Servidor'),
+        ('Chefe', 'Chefe')
+
+    ]
+    class Meta:
+        model = User
+        fields =['username','email','lastname','first_name']
+
+    username = forms.CharField(label='Matrícula: ')
+    email = forms.EmailField(label='Email: ')
+    last_name = forms.CharField(label='Nome Completo: ')
+    first_name = forms.ChoiceField(
+        label='Status: ',
+        choices=FIRST_NAME_CHOICES,
+        widget=forms.Select(attrs={'class':'custom-select'}),
+        initial='Aluno'
+    )
